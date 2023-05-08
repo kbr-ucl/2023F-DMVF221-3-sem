d238d5a2bae4339600efcaa6f4954d9f76e16c5e
KBR <kbr@ucl.dk>
2023-05-08 17:26:17 +0200

    CORS Demo

---
+++ 12-CORS/CorsApiDemo/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CorsApiDemo.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[EnableCors]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public string Get()
+    {
+        return "GET: Test message";
+    }
+
+    [HttpPost]
+    public string Post()
+    {
+        return "POST: Test message";
+    }
+
+    [HttpPut]
+    public string Put()
+    {
+        return "PUT: Test message";
+    }
+}
